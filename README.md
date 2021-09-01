fdc3702983d1cc1d2b7202bfbb27df5fb329bbe7
zhuzhilin1 <zhuzhilin1@jd.com>
2021-09-01 20:41:52 +0800

    commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Essentail JavaScript2 Vue-Grid
+
+## Build Setup
+
+Use the following commands to run the project.
+
+### install dependencies
+``` bash
+npm install
+```
+
+### Run Application
+
+For Development mode:
+
+``` bash
+npm run dev
+```
+
+For Production mode:
+
+```bash
+npm run build
+```
+
+The application will serve with hot reload at http://localhost:8080
+
+For detailed explanation on customization of grid. check our [documentation](https://ej2.syncfusion.com/vue/documentation/grid/getting-started.html)
---
+++ src/App.vue
@@ -0,0 +1,129 @@
+<template>
+  <div id="app">
+    <ejs-grid
+      :dataSource="data"
+      :allowSorting="true"
+      :allowFiltering="true"
+      :allowGrouping="true"
+    >
+      <e-columns>
+        <e-column
+          field="Inventor"
+          headerText="Inventor Name"
+          width="190"
+        ></e-column>
+        <e-column
+          field="NumberofPatentFamilies"
+          headerText="No of Patent Families"
+          width="120"
+        ></e-column>
+        <e-column
+          field="Country"
+          headerText="Country"
+          type="string"
+          :enableGroupByFormat="true"
+          :formatter="
+            (column, data) => (data.Country === 'Australia' ? 'Aus' : 'Other')
+          "
+          width="90"
+        ></e-column>
+        <e-column field="Active" headerText="Active" width="120"></e-column>
+        <e-column
+          field="Mainfieldsofinvention"
+          headerText="Main fields of invention"
+          width="200"
+        ></e-column>
+      </e-columns>
+    </ejs-grid>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+import {
+  GridPlugin,
+  Sort,
+  Filter,
+  Group,
+} from "@syncfusion/ej2-vue-grids";
+
+Vue.use(GridPlugin);
+
+export default {
+  data: function () {
+    return {
+      data: [
+        {
+          Inventor: "Kia Silverbrook",
+          NumberofPatentFamilies: 4737,
+          Country: "Australia",
+          "Number of INPADOC patents": 9839,
+          Active: "1994-2016",
+          createTime: "2021-08-10 12:30:42",
+          Mainfieldsofinvention:
+            "Printing, Digital paper, Internet, Electronics,Lab-on-a-chip, MEMS, Mechanical, VLSI",
+        },
+
+        {
+          Inventor: "Shunpei Yamazaki",
+          NumberofPatentFamilies: 4677,
+          Country: "Japan",
+          "Number of INPADOC patents": "10000+",
+          Active: "1976-2016",
+          createTime: "2021-08-10 12:30:42",
+          Mainfieldsofinvention:
+            "Thin film transistors, Liquid crystal displays, Solar cells, Flash memory, OLED",
+        },
+
+        {
+          Inventor: "Lowell L. Wood, Jr.",
+          NumberofPatentFamilies: 1419,
+          Country: "USA",
+          "Number of INPADOC patents": 1332,
+          Active: "1977-2016",
+          createTime: "2021-08-10 12:30:42",
+          Mainfieldsofinvention: "Mosquito laser, Nuclear weapons",
+        },
+
+        {
+          Inventor: "Paul Lapstun",
+          NumberofPatentFamilies: 1281,
+          Country: "Australia",
+          "Number of INPADOC patents": 3099,
+          Active: "2000-2016",
+          createTime: "2021-08-10 12:30:42",
+          Mainfieldsofinvention:
+            "Printing, Digital paper, Internet, Electronics, CGI, VLSI",
+        },
+
+        {
+          Inventor: "Gurtej Sandhu",
+          NumberofPatentFamilies: 1255,
+          Country: "India",
+          "Number of INPADOC patents": 2038,
+          Active: "1991-2016",
+          createTime: "2021-08-10 12:30:42",
+          Mainfieldsofinvention:
+            "Thin film processes and materials, VLSI, Semiconductor device fabrication",
+        },
+
+        {
+          Inventor: "Jun Koyama",
+          NumberofPatentFamilies: 1240,
+          Country: "Japan",
+          "Number of INPADOC patents": 4126,
+          Active: "1991-2016",
+          createTime: "2021-08-10 12:30:42",
+          Mainfieldsofinvention:
+            "Thin film transistors, Liquid crystal displays, OLED",
+        },
+      ],
+    };
+  },
+  provide: {
+    grid: [Sort, Filter, Group],
+  },
+};
+</script>
+<style>
+@import "../node_modules/@syncfusion/ej2-vue-grids/styles/material.css";
+</style>
